52f1fde60e59565078eb0296e9d74bffb0813e4f
Charul Dalvi <charuldalvi18@gmail.com>
2020-02-17 00:42:13 +0530

    order summary ui done

---
+++ app/src/main/java/com/ark/robokart_robotics/Fragments/Payment/OrderSummaryFragment.java
@@ -0,0 +1,33 @@
+package com.ark.robokart_robotics.Fragments.Payment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.ark.robokart_robotics.R;
+
+public class OrderSummaryFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.ordersummaryfragment,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    //Initialise UI
+    public void init(){}
+
+
+    //Event Listeners
+    public void listeners(){}
+}
